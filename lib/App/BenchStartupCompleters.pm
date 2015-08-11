a5a6e2f44682793827ce8cb3c4baf119c18db5e2
perlancar (@pc-office) <perlancar@gmail.com>
2015-08-11 16:23:31 +0700

    Initial commit

---
+++ lib/App/BenchStartupCompleters.pm
@@ -0,0 +1,11 @@
+package App::BenchStartupCompleters;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark startup time of some shell completers
+
+=head1 SYNOPSIS
+
+See the included script L<bench-startup-completers>.
